2b563e6f6a1f661e5cbb342a47399a94ac5c0d11
Graham <gblackledge88@gmail.com>
2024-09-02 13:56:19 +1200

    Test first pull request

--- PriceCheck/super/admin.py
+++ PriceCheck/super/admin.py
@@ -1,8 +1,4 @@
 from django.contrib import admin
-from .models import User, Supermarket, Product, Price
+
 
 # Register your models here.
-admin.site.register(User)
-admin.site.register(Supermarket)
-admin.site.register(Product)
-admin.site.register(Price)
