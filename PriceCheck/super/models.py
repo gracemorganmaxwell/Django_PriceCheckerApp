f86c1c4b66f2073a2230835b04a98c23b9282024
Declan Watts <declan@wattz.dev>
2024-09-09 17:01:02 +1200

    Models Fixed

--- PriceCheck/super/models.py
+++ PriceCheck/super/models.py
@@ -54,7 +54,7 @@ class Product(models.Model):
     store_id = models.ForeignKey(Store, on_delete=models.CASCADE)
     product_code = models.CharField(max_length=10, unique=True)
     unit_price = models.DecimalField(max_digits=10, decimal_places=2)
-    on_sale = models.BooleanField()
+    on_sale = models.BooleanField(null=True, default=None)
 
     def __str__(self):
         return str(self.product_name) + ": $" + str(self.unit_price)
@@ -66,7 +66,7 @@ class PriceHistory(models.Model):
     product_id = models.ForeignKey(Product, on_delete=models.CASCADE)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     date = models.DateField(auto_now_add=True)
-    on_sale = models.BooleanField()
+    on_sale = models.BooleanField(null=True, default=None)
 
     def __str__(self):
         return str(self.product_id) + ": $" + str(self.price)
