46138ad9935d69c584d9610bfa19785ad0e00555
Graham <gblackledge88@gmail.com>
2024-09-16 17:54:11 +1200

    improved login page

--- PriceCheck/super/urls.py
+++ PriceCheck/super/urls.py
@@ -11,7 +11,9 @@ from . import views
 urlpatterns = [
    path('signup/', register_view, name='signup' ),
     path('', HomePageView.as_view(), name='home'),
-    path('login/', auth_views.LoginView.as_view(authentication_form=EmailOrUsernameLoginForm), name='login'),
+    path('login/', auth_views.LoginView.as_view(
+        template_name='registration/login.html'  # Ensure correct path here
+    ), name='login'),    
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('store-preference/', store_preference_view, name='store_preference'),
     path('products/', product_list_view, name='product_list'),
