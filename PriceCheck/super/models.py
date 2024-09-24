15481d301e09f496b31ae83c2e382ceb6a670511
grace-morganmaxwell <gracemorganmaxwell@gmail.com>
2024-09-24 13:05:38 +1200

    intial commit

--- PriceCheck/super/models.py
+++ PriceCheck/super/models.py
@@ -148,10 +148,7 @@ class CartItem(models.Model):
 
     @property
     def total_price(self):
-        return self.quantity * self.product.price
-
-    def __str__(self):
-        return f"{self.quantity} x {self.product.name} for {self.user.username}"
+        return self.quantity * self.product.unit_price  # Change 'price' to 'unit_price'
     
 # def remove_from_cart(request, product_id):
 #     # Retrieve the cart from the session
